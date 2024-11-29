595b612120003808b4aa8a9dca0bbe86ad57858c
yuseihaneishi <yuseihaneishi@icloud.com>
2024-11-29 18:29:33 +0900

    11/29-2

---
+++ ft_strjoin.c
@@ -0,0 +1,44 @@
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	str = (char*)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+
+	str[j] = '\0';
+	return (str);
+}
+
+#include <stdio.h>
+
+int main() 
+{
+    char *result = ft_strjoin("Hello, ", "World!");
+    printf("%s\n", result);
+    free(result);
+    return 0;
+}
